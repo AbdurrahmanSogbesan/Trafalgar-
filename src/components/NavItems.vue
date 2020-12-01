7092ecaeaba2ab75a666826eaf9914f2cbdcea20
Abdurrahman Sogbesan <abdurrahman0803@gmail.com>
2020-12-01 15:43:27 +0100

    added header

---
+++ src/components/NavItems.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="nav-items">
+    <NavItem :link="link" v-for="(link, index) in navItems" :key="index"/>
+  </div>
+</template>
+
+<script>
+import NavItem from "./NavItem.vue";
+
+export default {
+    data() {
+        return {
+            navItems: [
+                {text: 'Home', isActive: true},
+                {text: 'Find a Doctor', isActive: false},
+                {text: 'Apps', isActive: false},
+                {text: 'Testimonials', isActive: false},
+                {text: 'About Us', isActive: false}
+            ]
+        }
+    },
+    components: {
+        NavItem,
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.nav-items {
+  display: flex;
+}
+</style>
